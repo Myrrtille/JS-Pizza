345265bb399a4b8aba10275169af0c2131dafe97
Myrrtille <shkutaanna@gmail.com>
2017-12-09 01:20:46 +0200

    HomeWork6_1

--- Backend/pages.js
+++ Backend/pages.js
@@ -5,16 +5,10 @@ exports.mainPage = function(req, res) {
     res.render('mainPage', {
         pageTitle: 'Вибір Піци'
     });
-    //document.getElementById('order-button').show();
-    //document.getElementById('cart-item-amount').hide();
-
 };
 
 exports.orderPage = function(req, res) {
     res.render('common/orderPage', {
         pageTitle: 'Замовлення'
     });
-
-   // document.getElementById('order-button').hide();
-    //document.getElementById('cart-item-amount').show();
 };
--- Frontend/src/main.js
+++ Frontend/src/main.js
@@ -5,11 +5,10 @@
 
 $(function(){
 
-
-
     //This code will execute when the page is ready
     var PizzaMenu = require('./pizza/PizzaMenu');
     var PizzaCart = require('./pizza/PizzaCart');
+    var map = require('./pizza/map');
     //var Pizza_List = require('./Pizza_List');
 
     PizzaCart.initialiseCart();
